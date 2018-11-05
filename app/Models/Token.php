6ba485887c5306d055141f00d2af5295c7026484
Yuri Chandra Triputra <yurichandra@gmail.com>
2018-11-05 11:55:57 +0700

    Added Token model.

---
+++ app/Models/Token.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+    /**
+     * Attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+}
