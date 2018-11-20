cc4ee6ae3465953e7c9f5362520b1c7ecd6e1fb5
Yuri Chandra Triputra <yurichandra@gmail.com>
2018-11-20 10:39:51 +0700

    Added user module

--- resources/js/store/modules/index.js
+++ resources/js/store/modules/index.js
@@ -1,9 +1,11 @@
 import LoggedUser from './LoggedUser'
 import Campaign from './Campaign'
 import School from './School'
+import User from './User'
 
 export default {
     LoggedUser,
     Campaign,
-    School
+    School,
+    User
 }
