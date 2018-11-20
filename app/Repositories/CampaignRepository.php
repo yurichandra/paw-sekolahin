3c808dd2d87b4c1ea50975d9ddb0a51aee58da86
Yuri Chandra Triputra <yurichandra@gmail.com>
2018-11-20 10:39:04 +0700

    Added find campaign by user

--- app/Repositories/CampaignRepository.php
+++ app/Repositories/CampaignRepository.php
@@ -3,8 +3,16 @@
 namespace App\Repositories;
 
 use App\Models\Campaign;
+use App\Models\User;
 
 class CampaignRepository extends BaseRepository
 {
     protected $model = Campaign::class;
+
+    public function findByUser($id)
+    {
+        $user = User::find($id);
+
+        return $user->campaigns;
+    }
 }
